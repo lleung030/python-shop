8a2b8d9b489c224143f6e8f5673c30ad5e89bdb7
lleung030 <lucasleung2593@gmail.com>
2023-09-06 13:28:09 -0700

    initial commit

---
+++ shop/__initi__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# # create the extension
+# db = SQLAlchemy()
+# # create the app
+# app = Flask(__name__)
+# # configure the SQLite database, relative to the app instance folder
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///myshop.db"
+# # initialize the app with the extension
+# db.init_app(app)
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///myshop.db'
+db = SQLAlchemy(app)
+
+from shop import routes
