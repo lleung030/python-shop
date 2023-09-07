a19fbab8200132b90039f32e53346276f1396bbd
lleung030 <lucasleung2593@gmail.com>
2023-09-07 14:57:15 -0700

    model add product

--- shop/products/forms.py
+++ shop/products/forms.py
@@ -1,14 +1,15 @@
-from flask_wtf import FileAllowed, FileField, FileRequired
-from wtforms import Form, IntegerField, StringField, BooleanField, TextAreaField, validators
+from wtforms import Form, SubmitField,IntegerField,FloatField,StringField,TextAreaField,validators
+from flask_wtf.file import FileField,FileRequired,FileAllowed
+from flask_wtf import FlaskForm 
 
 class Addproducts(Form):
     name = StringField('Name', [validators.DataRequired()])
-    price = IntegerField('Price', [validators.DataRequired()])
+    price = FloatField('Price', [validators.DataRequired()])
     discount = IntegerField('Discount', default=0)
     stock = IntegerField('Stock', [validators.DataRequired()])
+    colors = StringField('Colors', [validators.DataRequired()])
     description = TextAreaField('Description', [validators.DataRequired()])
-    colors = TextAreaField('Colors', [validators.DataRequired()])
 
-    image_1 = FileField('Image 1', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'gif', 'jpeg']), 'images only please!'])
-    image_2 = FileField('Image 2', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'gif', 'jpeg']), 'images only please!'])
-    image_3 = FileField('Image 3', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'gif', 'jpeg']), 'images only please!'])
+    image_1 = FileField('Image 1', validators=[FileRequired(), FileAllowed(['jpg','png','gif','jpeg'], 'Images only')])
+    image_2 = FileField('Image 2', validators=[FileRequired(), FileAllowed(['jpg','png','gif','jpeg'], 'Images only')])
+    image_3 = FileField('Image 3', validators=[FileRequired(), FileAllowed(['jpg','png','gif','jpeg'], 'Images only')])
