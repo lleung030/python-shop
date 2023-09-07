fa8fe6e5ae586c0729ef572487fdb37da5e3bf13
lleung030 <lucasleung2593@gmail.com>
2023-09-07 12:10:48 -0700

    add a category or brand

--- shop/__init__.py
+++ shop/__init__.py
@@ -2,15 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
-# # create the extension
-# db = SQLAlchemy()
-# # create the app
-# app = Flask(__name__)
-# # configure the SQLite database, relative to the app instance folder
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///myshop.db"
-# # initialize the app with the extension
-# db.init_app(app)
-
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///myshop.db'
 app.config['SECRET_KEY'] = 'ouehdvouiqernhhvoweirn'
@@ -18,3 +9,4 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
 from shop.admin import routes
+from shop.products import routes
