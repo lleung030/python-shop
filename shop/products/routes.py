ee6907eb1531eb5072713cb8910b0139c48d43bb
lleung030 <lucasleung2593@gmail.com>
2023-09-07 12:48:27 -0700

    add a product

--- shop/products/routes.py
+++ shop/products/routes.py
@@ -1,13 +1,15 @@
 from flask import redirect, render_template, url_for, flash, request
 from shop import db, app
 from .models import Brand, Category
+from .forms import Addproducts
+import secrets 
 
 @app.route('/addbrand', methods=['GET', 'POST'])
 def addbrand():
     if request.method=='POST':
         getbrand = request.form.get('brand')
         brand = Brand(name=getbrand)
-        db.session.add(brand)
+        db.session.add(brand) 
         flash(f'The Brand {getbrand} was added to your databse', 'success')
         db.session.commit()
         return redirect(url_for('addbrand'))
@@ -25,3 +27,8 @@ def addcat():
         return redirect(url_for('addcat'))
     
     return render_template('products/addbrand.html')
+
+@app.route('/Addproduct', methods=['GET', 'POST'])
+def addproduct():
+    form = Addproducts(request.form)
+    return render_template('products/addproduct.html', title='Add Product Page', form=form)
